a3e8410b18f6967ab54b4cfb0927ef232349d8e3
Dmitrii <richard.orlov13@gmail.com>
2024-02-04 22:06:14 +0300

    Домашка с экранами и тостами

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val buttonSearch: Button = findViewById(R.id.button_search)
+        val buttonMedia: Button = findViewById(R.id.button_media)
+        val buttonSettings: Button = findViewById(R.id.button_settings)
+
+        buttonSearch.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                showToast("Search button clicked")
+            }
+        })
+
+        buttonMedia.setOnClickListener {
+            showToast("Media button clicked")
+        }
+
+        buttonSettings.setOnClickListener {
+            showToast("Settings button clicked")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
