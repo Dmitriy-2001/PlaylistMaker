b3612b73649884e4d52607a33ccfc6c3c923a800
Dmitrii <richard.orlov13@gmail.com>
2024-03-12 21:20:35 +0300

    10 спринт окончательный 2

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,35 +1,34 @@
 package com.example.playlistmaker
 
+import android.content.res.Resources
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.material.internal.ViewUtils.dpToPx
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    private val trackName: TextView
-    private val artistName: TextView
-    private val trackTime: TextView
-    private val artworkUrl100: ImageView
-
-    init {
-        trackName = itemView.findViewById(R.id.trackName)
-        artistName = itemView.findViewById(R.id.artistName)
-        trackTime = itemView.findViewById(R.id.trackTime)
-        artworkUrl100 = itemView.findViewById(R.id.trackImage)
-    }
+    val trackName: TextView = itemView.findViewById(R.id.trackName)
+    val artistName: TextView = itemView.findViewById(R.id.artistName)
+    val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+    val artworkUrl100: ImageView = itemView.findViewById(R.id.trackImage)
 
     fun bind(track: Track) {
         trackName.text = track.trackName
         artistName.text = track.artistName
-        artistName.text = track.artistName
         trackTime.text = track.trackTime
-        Glide.with(itemView).load(track.artworkUrl100)
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
             .placeholder(R.drawable.search)
             .centerCrop()
-            .transform(RoundedCorners(2))
+            .transform(RoundedCorners(dpToPx(2)))
             .into(artworkUrl100)
     }
 }
+    fun dpToPx(dp: Int): Int {
+        val density = Resources.getSystem().displayMetrics.density
+        return (dp * density).toInt()
+    }
+
